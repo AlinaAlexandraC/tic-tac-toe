253a9746d28f54fbaa89d9708c17d94702665f90
github username <tabarceagheorghe0603@gmail.com>
2023-05-27 14:56:00 +0300

    By George

--- do-not-modify-this-file.js
+++ do-not-modify-this-file.js
@@ -74,6 +74,7 @@ function setHTMLvisibilityForInputAiCoordinatesInput(isVisible) {
     }
 }
 
+
 // isVisible = true  - will show the element
 // isVisible = false - will hide the element
 function setHTMLvisibilityForButtonLabeledReset(isVisible) {
